50a7d58b573ce6a6d45c473acecd8dcc0a8edd20
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2022-05-07 11:54:28 +0530

    Add colors to the board

--- app/src/main/java/com/example/twentyfourtyeight/MainActivity.kt
+++ app/src/main/java/com/example/twentyfourtyeight/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.KeyEvent
 import android.view.MotionEvent
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import com.example.twentyfourtyeight.core.Game
 import com.example.twentyfourtyeight.core.Tile
 import androidx.core.view.GestureDetectorCompat
@@ -25,8 +26,11 @@ class MainActivity : AppCompatActivity() {
 
     private fun onTileChangeListener(tile: Tile) {
         val id = resources.getIdentifier("row_${tile.getPosition().x}_${tile.getPosition().y}", "id", applicationContext.packageName)
-        val view = findViewById<TextView>(id)
-        view.text = tile.getStringifiedValue()
+        val textView = findViewById<TextView>(id)
+        val card = findViewById<TextView>(id).parent as CardView
+        textView.text = tile.getStringifiedValue()
+        textView.setTextColor(resources.getColor(resources.getIdentifier("value_${tile.getStringifiedValue()}_fg", "color", packageName), null))
+        card.setCardBackgroundColor(resources.getColor(resources.getIdentifier("value_${tile.getStringifiedValue()}_bg", "color", packageName), null))
     }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
